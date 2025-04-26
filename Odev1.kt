389167ab9e4adcb7a543a3fa3ccf6d3f30be7bd2
mrtaydn <mertaydin017@gmail.com>
2025-04-26 17:27:33 +0300

    Odev1

---
+++ Odev1.kt
@@ -0,0 +1,143 @@
+package com.example.kotlindersleri.Odevler
+
+fun main(){
+    val sehir = "Samsun"
+    println("Sehir = $sehir")
+
+    val ulke = "Türkiye"
+    println("Ülke = $ulke")
+
+    val telefon = "Iphone"
+    println("Telefon = $telefon")
+
+    val postaKodu = "34000"
+    println("Posta Kodu = $postaKodu")
+
+    val email = "mertaydin017@gmail.com"
+    println("Email = $email")
+
+    val meslek = "Yazılım Mühendisi"
+    println("Meslek = $meslek")
+
+    val stokMiktari = "1000"
+    println("Stok Miktarı = $stokMiktari")
+
+    val musteriAdi = "Mert Aydın"
+    println("Müşteri Adı = $musteriAdi")
+
+    val bakiye = "100 TL"
+    println("Bakiye = $bakiye")
+
+    val dogumGunu = "11.01.1999"
+    println("Doğum Günü = $dogumGunu")
+
+    val maas = "35.000 Tl"
+    println("Maaş = $maas")
+
+    val medeniDurum = "Bekar"
+    println("Medeni Durum = $medeniDurum")
+
+    val urunYorum = "Güzel bir ürün herkese tavsiye ederim."
+    println("Ürün Yorum = $urunYorum")
+
+    val odemeTarihi = "25.04.2025"
+    println("Ödeme Tarihi = $odemeTarihi")
+
+    val odeme = "2.500 Tl"
+    println("Ödeme = $odeme")
+
+    val siparisAdeti = "5 Adet"
+    println("Sipariş Adeti = $siparisAdeti")
+
+    val arabaModeli = "Volkswagen - Golf"
+    println("Araba Modeli = $arabaModeli")
+
+    val kitapAdi = "Nutuk"
+    println("Kitap Adı = $kitapAdi")
+
+    val yayinlamaTarihi = "23.04.2025"
+    println("Yayınlama Tarihi = $yayinlamaTarihi")
+
+    val indirimMiktari = "%50"
+    println("İndirim Miktarı = $indirimMiktari")
+
+    val odaSayisi = "3"
+    println("Oda Sayısı = $odaSayisi")
+
+    val enlem = "36–42° Kuzey"
+    println("Enlem = $enlem")
+
+    val boylam = "26–45° Doğu"
+    println("Boylam = $boylam")
+
+    val urunAdi = "Plaj Terliği"
+    println("Ürün Adı = $urunAdi")
+
+    val yemekFiyati = "250 TL"
+    println("Yemek Fiyatı = $yemekFiyati")
+
+    var marka = "Adidas"
+    println("Marka = $marka")
+
+    var muzikAdi = "Volkan Konak - Ömrüm"
+    println("Müzik Adı = $muzikAdi")
+
+    var videoSuresi = "29.54"
+    println("Video Süresi = $videoSuresi")
+
+    var urunPuani = "4.5"
+    println("Ürün Puanı = $urunPuani")
+
+    var resimAdi = "Doğa Resmi"
+    println("Resim Adı = $resimAdi")
+
+    var dosyaFormati = "PDF"
+    println("Dosya Formatı = $dosyaFormati")
+
+    var renk = "Kırmızı"
+    println("Renk = $renk")
+
+    var renkKodu = "#ff0000"
+    println("Renk Kodu = $renkKodu")
+
+    var telefonModeli = "Iphone 14 Pro Max"
+    println("Telefon Modeli = $telefonModeli")
+
+    var ekranBoyutu = "55 inç"
+    println("Ekran Boyutu = $ekranBoyutu")
+
+    var agirlik = "130 Kg"
+    println("Ağırlık = $agirlik")
+
+    var ulusalGun = "23 Nisan Ulusal Egemenlik ve Çocuk Bayramı"
+    println("Ulusal Gün = $ulusalGun")
+
+    var tatilGunu = "Pazar"
+    println("Tatil Günü = $tatilGunu")
+
+    var rezervasyonTarihi = "24.04.2025"
+    println("Rezervasyon Tarihi = $rezervasyonTarihi")
+
+    var sokakAdi = "Şehit Er Davut Baş Sokak"
+    println("Sokak Adı = $sokakAdi")
+
+    var otobusHatti = "93M"
+    println("Otobüs Hattı = $otobusHatti")
+
+    var kalanDakika = "25 Dakika"
+    println("Kalan Dakika = $kalanDakika")
+
+    var takipKodu = "TR1234567890XX"
+    println("Takip Kodu = $takipKodu")
+
+    var kuponSuresi = "15 Gün"
+    println("Kupon Süresi = $kuponSuresi")
+
+    var kuponKodu = "HOSGELDİN100"
+    println("Kupon Kodu = $kuponKodu")
+
+    var faturaAdresi = "Telsiz Mah. No:10 Zeytinburnu/İSTANBUL"
+    println("Fatura Adresi = $faturaAdresi")
+
+
+}
